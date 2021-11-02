322f6a9a2cfb86aec7f2cdad2db87e58faf15bb8
AleksandrTsyhankov <74210910+AleksandrTsyhankov@users.noreply.github.com>
2021-11-02 23:19:45 +0200

    init commit hw5

---
+++ src/com/pb/tsygankov/hw5/Library.java
@@ -0,0 +1,47 @@
+package com.pb.tsygankov.hw5;
+
+public class Library {
+    public static void main(String[] args) {
+        Book[] books = new Book[3];
+        books[0] = new Book("Приключения", "Иванов И.И.", 2000);
+        books[1] = new Book("Словарь", "Сидоров А.В.", 1980);
+        books[2] = new Book("Энциклопедия", "Гусев К.В.", 2010);
+
+        Reader[] readers = new Reader[3];
+        readers[0] = new Reader("Петров", 1, "Исторический", "01.01.1980", "0671232233");
+        readers[1] = new Reader("Иванов", 2, "Математический", "01.01.1990", "0633213322");
+        readers[2] = new Reader("Сидоров", 3, "Экономический", "01.01.3000", "0501110022");
+
+        System.out.println("Список всех книг: ");
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(" Название: "+books[i].getTitle()+", Автор: "+books[i].getAuthor()+", Год: "+books[i].getYear());
+        }
+
+        System.out.println("Список всех читателей: ");
+        for (int i = 0; i < readers.length; i++) {
+            System.out.println(" ФИО: "+readers[i].getFio()+", Номер: "+readers[i].getReaderNumber()+", Факультет: "+readers[i].getFaculty()+", Дата рождения: "+readers[i].getDateOfBirth()+", Номер телефона: "+readers[i].getPhoneNumber());
+        }
+
+        String[] authors = new String[3];
+        authors[0] = books[0].getTitle();
+        authors[1] = books[1].getTitle();
+        authors[2] = books[2].getTitle();
+
+        readers[0].takeBooks(books.length);
+        readers[0].returnBooks(books.length);
+
+        readers[0].takeBooks(authors);
+        readers[0].returnBooks(authors);
+
+        readers[0].takeBooks(books);
+        readers[0].returnBooks(books);
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/com/pb/tsygankov/hw5/Reader.java
@@ -0,0 +1,107 @@
+package com.pb.tsygankov.hw5;
+
+public class Reader {
+    private String fio;
+    private int readerNumber;
+    private String faculty;
+    private String dateOfBirth;
+    private String phoneNumber;
+
+    public Reader(String fio, int readerNumber, String faculty, String dateOfBirth, String phoneNumber) {
+        this.fio = fio;
+        this.readerNumber = readerNumber;
+        this.faculty = faculty;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public int getReaderNumber() {
+        return readerNumber;
+    }
+
+    public void setReaderNumber(int readerNumber) {
+        this.readerNumber = readerNumber;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void takeBooks(int booksCount) {
+        System.out.println(fio+" взял "+booksCount+" книгу(и).");
+    }
+
+    public void returnBooks(int booksCount) {
+        System.out.println(fio+" вернул "+booksCount+" книгу(и).");
+    }
+
+    public void takeBooks(String[] authors) {
+        String books = "";
+
+        for (int i = 0; i < authors.length; i++) {
+            books += authors[i]+", ";
+        }
+        books = books.substring(0, books.length()-2)+".";
+        System.out.println(fio + " взял книгу(и): " + books);
+    }
+
+    public void returnBooks(String[] authors) {
+        String books = "";
+
+        for (int i = 0; i < authors.length; i++) {
+            books += authors[i] +  ", ";
+        }
+        books = books.substring(0, books.length()-2)+".";
+        System.out.println(fio + " вернул книгу(и): " + books);
+    }
+
+    public void takeBooks(Book[] books) {
+        String booksInfo = "";
+
+        for (int i = 0; i < books.length; i++) {
+            booksInfo += books[i].getTitle() + " (" + books[i].getAuthor() + " " + books[i].getYear() + " г.), ";
+
+        }
+        booksInfo = booksInfo.substring(0, booksInfo.length()-2)+".";
+        System.out.println(fio + " взял книгу(и): " + booksInfo);
+    }
+
+    public void returnBooks(Book[] books) {
+        String booksInfo = "";
+
+        for (int i = 0; i < books.length; i++) {
+            booksInfo += books[i].getTitle() + " (" + books[i].getAuthor() + " " + books[i].getYear() + " г.), ";
+
+        }
+        booksInfo = booksInfo.substring(0, booksInfo.length()-2)+".";
+        System.out.println(fio + " взял книгу(и): " + booksInfo);
+    }
+}
